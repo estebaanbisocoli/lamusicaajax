2d0e44be7957000620a0f1f188bc6cc81142c2de
esteban <estebaanbisocoli@gmail.com>
2018-09-09 17:00:37 -0300

    all working, needs refactor

--- index.js
+++ index.js
@@ -14,49 +14,88 @@ JQuery.ajax({
           return 5;
       }
     }
+    function mapDayOfTheWeek(numberDay) {
+      const daysOfTheWeek = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday"
+      ];
+
+      return daysOfTheWeek[numberDay];
+    }
     function converTimezone(currentDate, hourOffset) {
       const MILLI_HOUR = 3600000;
-      const d = new Date(currentDate.valueOf() - 3600000 * hourOffset);
+      const d = new Date(currentDate.valueOf() - MILLI_HOUR * hourOffset);
       return d;
     }
     function getUTCHourOffset(date, serverOffset) {
       return parseInt(serverOffset - date.getTimezoneOffset() / 60);
     }
-    function getShowDate(currentDate, serverOffset) {
-      return converTimezone(
-        currentDate,
-        getUTCHourOffset(currentDate, serverOffset)
-      );
-    }
-    function newLocalSchedule(schedule, timediff, dayOfTheWeek) {
-      return;
+    function converToAMPM(hour24) {
+      if (hour24 > 24) {
+        hour24 = hour24 - 24;
+      } else if (hour24 < 0) {
+        hour24 = hour24 + 24;
+      }
+      console.log(hour24);
+      if (hour24 <= 12) {
+        return hour24 + ":00 AM";
+      } else {
+        return hour24 - 12 + ":00 PM";
+      }
     }
     const theme_name = "wxdj";
     const currentRadio = result.data.find(radio => {
       return radio.slug == theme_name;
     });
-
-    // console.log(currentRadio.programming.times);
-    // const CurrentDateGMT = -getCurrrentGMT();
-    // const timezone = getGMT(currentRadio.programming.timezone);
-    // console.log(getTimeDiff(timezone, CurrentDateGMT));
-    // const localSchedule = newLocalSchedule();
     const currentDate = new Date(Date.now());
-    console.log(getGMT(currentRadio.programming.timezone));
-    console.log(
-      getShowDate(currentDate, getGMT(currentRadio.programming.timezone))
+    const serverOffset = getGMT(currentRadio.programming.timezone);
+    const serverDate = converTimezone(
+      currentDate,
+      getUTCHourOffset(currentDate, serverOffset)
     );
+
+    const dayString = mapDayOfTheWeek(serverDate.getDay());
+    const showsOfTheWeek = currentRadio.programming.times;
+    const showsOfTheDay = showsOfTheWeek[dayString];
+    const currentShowServerTime = showsOfTheDay.find(show => {
+      return (
+        serverDate.getHours() >= show.from && serverDate.getHours() < show.to
+      );
+    });
+    console.log(currentRadio);
+    const displayObj = {
+      from:
+        currentShowServerTime.from +
+        getUTCHourOffset(currentDate, serverOffset),
+      to:
+        currentShowServerTime.to + getUTCHourOffset(currentDate, serverOffset),
+      title: currentShowServerTime.radio_name,
+      link: currentRadio.programming.radio_url,
+      image: currentShowServerTime.live_img_url
+    };
+
+    console.log(displayObj);
     JQuery(".getTime").html(`<div class="programming-editor-container getTime">
     <div class="active-dj-title">
-        <a href="http://www.lamusica.com/sbsplayer-elzol.php" onclick="NewWindow(this.href,'name','850','850','no');return false">
+        <a href="${
+          displayObj.link
+        }" onclick="NewWindow(this.href,'name','850','850','no');return false">
             <span>ESCUCHA EN VIVO</span>
         </a>
-        <p>Pura Música<br>1:00 PM - 6:00 PM</p>
+        <p>Pura Música<br>${converToAMPM(displayObj.from)} - ${converToAMPM(
+      displayObj.to
+    )}</p>
     </div>
     
-    <a href="http://www.lamusica.com/sbsplayer-elzol.php" onclick="NewWindow(this.href,'name','850','850','no');return false">
+    <a href="${
+      displayObj.link
+    }" onclick="NewWindow(this.href,'name','850','850','no');return false">
         <div class="active-dj-headshot">
-            <img src="https://static.lamusica.com/wp-content/themes/sbsradio2014/images/envivo-logo.jpg">
+            <img src="${displayObj.image}">
         </div>
     </a>
 </div>`);
