11a7068f8233e5c6d73eda1edfdbd2fd642481eb
esteban bisocoli <estebaanbisocoli@gmail.com>
2018-09-09 18:41:43 -0300

    not working. checkout last commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // "http://api.lamusica.com/audio/content/stations?page_size=20"
 const JQuery = $;
+function getGMT(timezoneStr) {
+  switch (timezoneStr) {
+    case "America/New_York" || "America/Puerto_Rico":
+      return 4;
+    case "America/Los_Angeles":
+      return 7;
+    case "America/Chicago":
+      return 5;
+  }
+}
 
 JQuery.ajax({
   url: "http://api.lamusica.com/audio/content/stations?page_size=20",
@@ -15,43 +25,51 @@ JQuery.ajax({
       }
     }
     function converTimezone(currentDate, hourOffset) {
+      console.log(hourOffset, "offset convertTimezone");
       const MILLI_HOUR = 3600000;
-      const d = new Date(currentDate.valueOf() - 3600000 * hourOffset);
+      const d = new Date(currentDate.valueOf() - MILLI_HOUR * hourOffset);
       return d;
     }
-    function getUTCHourOffset(date, serverOffset) {
-      return parseInt(serverOffset - date.getTimezoneOffset() / 60);
+    function dayToString(dayOfTheWeek) {
+      const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+      ];
+      return days[dayOfTheWeek];
     }
-    function getShowDate(currentDate, serverOffset) {
-      return converTimezone(
-        currentDate,
-        getUTCHourOffset(currentDate, serverOffset)
-      );
-    }
-    function newLocalSchedule(schedule, timediff, dayOfTheWeek) {
-      return;
-    }
+    function localeTime(from, to, offset) {}
     const theme_name = "wxdj";
     const currentRadio = result.data.find(radio => {
       return radio.slug == theme_name;
     });
 
-    // console.log(currentRadio.programming.times);
-    // const CurrentDateGMT = -getCurrrentGMT();
-    // const timezone = getGMT(currentRadio.programming.timezone);
-    // console.log(getTimeDiff(timezone, CurrentDateGMT));
-    // const localSchedule = newLocalSchedule();
+    const shows = currentRadio.programming;
     const currentDate = new Date(Date.now());
-    console.log(getGMT(currentRadio.programming.timezone));
-    console.log(
-      getShowDate(currentDate, getGMT(currentRadio.programming.timezone))
-    );
+    const serverTimezoneName = shows.timezone;
+    const serverDate = converTimezone(currentDate, getGMT(serverTimezoneName));
+    const dayOfTheWeek = dayToString(serverDate.getDay());
+    const serverHour = serverDate.getHours();
+    console.log(currentDate, serverDate);
+    const offset = currentDate.getHours() - serverDate.getHours();
+    console.log(offset);
+    const show = shows.times[dayOfTheWeek].find(show => {
+      return serverHour >= show.from && serverHour < show.to;
+    });
+    console.log(show);
+    console.log(shows);
     JQuery(".getTime").html(`<div class="programming-editor-container getTime">
     <div class="active-dj-title">
-        <a href="http://www.lamusica.com/sbsplayer-elzol.php" onclick="NewWindow(this.href,'name','850','850','no');return false">
+        <a href="${
+          shows.radio_url
+        }" onclick="NewWindow(this.href,'name','850','850','no');return false">
             <span>ESCUCHA EN VIVO</span>
         </a>
-        <p>Pura Música<br>1:00 PM - 6:00 PM</p>
+        <p>${show.radio_name}<br>${""}</p>
     </div>
     
     <a href="http://www.lamusica.com/sbsplayer-elzol.php" onclick="NewWindow(this.href,'name','850','850','no');return false">
